9362a4261b6281234a34a4a7ef9d4b934f248ef8
Frederik Lux <f.lux@phytec.de>
2022-06-22 12:31:16 +0200

    update facility sql with own database;

--- facility-mysql/database/test_facility_v1-0-1.sql
+++ facility-mysql/database/test_facility_v1-0-1.sql
@@ -1,20 +1,20 @@
 SET FOREIGN_KEY_CHECKS = 0;
 
-INSERT IGNORE INTO `facility_code_pool`
-SELECT * FROM `fake_code_pool`;
+INSERT IGNORE INTO `facility-dev`.`facility_code_pool`
+SELECT * FROM `rhea-dev`.`fake_code_pool`;
 
-INSERT IGNORE INTO `facility_stack_detail`
-SELECT * FROM `fake_stack_detail`;
+INSERT IGNORE INTO `facility-dev`.`facility_stack_detail`
+SELECT * FROM `rhea-dev`.`fake_stack_detail`;
 
-INSERT IGNORE INTO `facility_stack_image`
-SELECT * FROM `fake_stack_image`;
+INSERT IGNORE INTO `facility-dev`.`facility_stack_image`
+SELECT * FROM `rhea-dev`.`fake_stack_image`;
 
-DROP TABLE `fake_code_pool`;
-DROP TABLE `fake_stack_image`;
-DROP TABLE `fake_stack_detail`;
+DROP TABLE `rhea-dev`.`fake_code_pool`;
+DROP TABLE `rhea-dev`.`fake_stack_image`;
+DROP TABLE `rhea-dev`.`fake_stack_detail`;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
-UPDATE `rhea_settings`
-SET `setting_value` = 0
-WHERE `setting` = 'use_phptool';
+UPDATE `rhea-dev`.`rhea_settings`
+SET `rhea-dev`.`setting_value` = 0
+WHERE `rhea-dev`.`setting` = 'use_phptool';
