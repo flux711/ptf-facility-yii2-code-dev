ba699dd5394134c991b04536eade6dc11033e50e
Frederik Lux <f.lux@phytec.de>
2022-06-27 10:28:29 +0200

    sql script for development system

--- import-tables.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-SCRIPT_PATH=$(dirname $(realpath -s $0))
-for SQL in $SCRIPT_PATH/facility-mysql/database/*.sql; do docker exec -i rhea_mysql_1 mysql -uroot -prhea1 rhea-dev < $SQL; done
